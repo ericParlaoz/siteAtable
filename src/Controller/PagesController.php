d1babae23426c608d8eb6640512c56b182fc78a6
eric <Rextoutou38??>
2022-04-12 21:16:35 +0200

    Mise en page

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\FichiersRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    #[Route('/', name: 'app_accueil')]
+    public function index(FichiersRepository $fichiersRepository): Response
+    {
+
+       $fichier = $fichiersRepository->findBy([], ['id' => 'DESC'], 1);
+       $path = $fichier[0]->getFilename();
+
+        return $this->render('pages/index.html.twig',[
+            'lienMenu' => $path
+        ]);
+    }
+
+    #[Route('/menu', name: 'app_menu')]
+    public function menu(FichiersRepository $fichiersRepository): Response
+    {
+
+        $fichier = $fichiersRepository->findBy([], ['id' => 'DESC'], 1);
+        $path = $fichier[0]->getFilename();
+
+        return $this->render('pages/menu.html.twig',[
+            'lienMenu' => $path
+        ]);
+    }
+
+    #[Route('/jardin', name: 'app_jardin')]
+    public function jardin(): Response
+    {
+
+
+
+        return $this->render('pages/jardin.html.twig');
+    }
+
+
+}
